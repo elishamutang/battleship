1df969a6a8e90eae3fe518d1220f3175554dade9
elishamutang <elishamutang@outlook.com>
2024-07-10 23:21:58 +0800

    [update] Added shipType method to identify type of ship for each Ship object.

--- src/ship.js
+++ src/ship.js
@@ -3,6 +3,7 @@ export class Ship {
         this.length = length
         this.hitsTaken = 0
         this.sunk = false
+        this.typeOfShip = this.shipType()
     }
 
     hit() {
@@ -16,7 +17,17 @@ export class Ship {
     isSunk() {
         this.sunk = true
     }
-}
 
-const test = new Ship(3)
-console.log(test)
+    shipType() {
+        switch (this.length) {
+            case 2:
+                return 'patrol boat'
+            case 3:
+                return 'destroyer'
+            case 4:
+                return 'battleship'
+            case 5:
+                return 'carrier'
+        }
+    }
+}
