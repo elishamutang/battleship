b1362d0a619f87c4ebde0009ec537f1000e67d19
elishamutang <elishamutang@outlook.com>
2024-07-11 23:42:48 +0800

    [update] Updated receiveAttack method.

--- src/gameboard.js
+++ src/gameboard.js
@@ -15,8 +15,9 @@ export class Gameboard {
         return board
     }
 
+    // Place ship on gameboard
     placeShip(ship, coord) {
-        // Place ship on gameboard
+        // Keeps track of current ships on gameboard.
         this.ships.push(ship)
 
         let [x, y] = coord
@@ -24,7 +25,7 @@ export class Gameboard {
         if (x > 10 || y > 10) throw new Error('Out of bounds')
 
         // Starting coordinate, depending on the type of ship, the ship will populate a number of coordinates
-        // where the total number of coordinates occupied equals to its length.
+        // where the total number of boxes occupied equals to its length.
         this.board[x][y] = ship.typeOfShip.split('')[0]
 
         let finalX = x + ship.length
@@ -40,9 +41,31 @@ export class Gameboard {
                 this.board[x][y + i] = ship.typeOfShip.split('')[0]
             }
         }
+
+        // Drag and drop option
+        //...
     }
 
-    receiveAttack() {
-        // Should take a pair of coordinates, determines if the ship is attacked or not
+    // Receives a pair of coordinates and determines if any ships were attacked.
+    receiveAttack(coord) {
+        const [x, y] = coord
+
+        const [ship] = this.ships.filter((elem) => {
+            if (elem.typeOfShip.split('')[0] === this.board[x][y]) {
+                return elem
+            }
+        })
+
+        // If the coordinate is occupied by a type of ship, add it to hitsTaken property.
+        if (ship) {
+            ship.hitsTaken += 1
+
+            // If hitsTaken equals to ship length then ship has sunken.
+            if (ship.hitsTaken === ship.length) {
+                ship.sunk = true
+            }
+        }
+
+        this.board[x][y] = 'x'
     }
 }
