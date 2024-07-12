62c114a2ac71adafc04123a277ec115637838106
elishamutang <elishamutang@outlook.com>
2024-07-12 22:27:25 +0800

    [update] Constructed player gameboards.

--- src/theDOM.js
+++ src/theDOM.js
@@ -2,4 +2,52 @@ import { Ship } from './ship'
 import { Gameboard } from './gameboard'
 import { Player } from './player'
 
-export default function generateTheDOM() {}
+export default function generateTheDOM() {
+    // Initialize players
+    const realPlayer = new Player()
+    const computerPlayer = new Player()
+
+    // Generate gameboard
+    const realPlayerGameboard = document.getElementById('realPlayer')
+    const computerPlayerGameboard = document.getElementById('computerPlayer')
+
+    generateGameboard(realPlayer, realPlayerGameboard)
+    generateGameboard(computerPlayer, computerPlayerGameboard)
+}
+
+function generateGameboard(player, playerGameboard) {
+    // Construct gameboard
+    player.gameboard.board.forEach((row) => {
+        const rowDiv = document.createElement('div')
+        rowDiv.className = 'row'
+
+        playerGameboard.append(rowDiv)
+
+        row.forEach(() => {
+            const location = document.createElement('div')
+            location.className = 'loc'
+
+            rowDiv.append(location)
+        })
+    })
+
+    const getAllRowDivs = Array.from(playerGameboard.getElementsByClassName('row'))
+
+    // Numbering
+    getAllRowDivs.forEach((div, idx) => {
+        if (idx !== 0) {
+            div.children[0].textContent = idx
+            div.children[0].className += ' numbering'
+        }
+    })
+
+    // Alphabets
+    const alphabets = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ']
+
+    Array.from(getAllRowDivs[0].children).forEach((loc, idx) => {
+        if (idx !== 0) {
+            loc.textContent = alphabets[idx - 1]
+            loc.className += ' alphabets'
+        }
+    })
+}
