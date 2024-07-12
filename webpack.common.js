13ed5857abdd1e3ec69baff026084231caec1297
elishamutang <elishamutang@outlook.com>
2024-07-12 21:47:18 +0800

    [update] Added title.

--- webpack.common.js
+++ webpack.common.js
@@ -39,7 +39,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: '',
+            title: 'Battleship!',
             filename: 'index.html',
             template: 'src/template.html',
         }),
