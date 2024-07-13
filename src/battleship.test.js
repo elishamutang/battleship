9b1ef2b3a13ccd54cc3c9c71b94de6016c10f1d6
elishamutang <elishamutang@outlook.com>
2024-07-13 17:37:02 +0800

    [update] Remove redundant test.

--- src/battleship.test.js
+++ src/battleship.test.js
@@ -161,12 +161,5 @@ describe('Player class', () => {
         expect(Object.keys(playerOne.gameboard)).toEqual(['board', 'ships', 'areAllShipsSunked'])
     })
 
-    it.skip('Record hits on gameboard', () => {
-        const coord = [0, 0]
-        playerOne.gameboard.receiveAttack(coord)
-
-        expect(playerOne.gameboard.board[coord[0]][coord[1]]).toBe('x')
-    })
-
     it.todo('Record hits on ship')
 })
