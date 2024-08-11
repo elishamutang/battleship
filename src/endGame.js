e88d0d3928a6a2d70a7fe8baa8f41124f9309bff
elishamutang <elishamutang@outlook.com>
2024-08-11 16:24:13 +0800

    [update] Added end game banner when the game ends.

--- src/endGame.js
+++ src/endGame.js
@@ -1,9 +1,4 @@
 export default function endGame(realPlayerGameboard, computerPlayerGameboard) {
-    // Announce the winner
-    setTimeout(() => {
-        alert('You won!')
-    }, 200)
-
     // Disable gameboards.
     const allTiles = Array.from(computerPlayerGameboard.getElementsByClassName('loc'))
 
@@ -13,6 +8,15 @@ export default function endGame(realPlayerGameboard, computerPlayerGameboard) {
 
     computerPlayerGameboard.className = computerPlayerGameboard.className + ' gameover'
     realPlayerGameboard.className = realPlayerGameboard.className + ' gameover'
+
+    // Add end game banner
+    const mainDiv = document.querySelector('main')
+
+    const endGameBanner = document.createElement('div')
+    endGameBanner.id = 'endGameBanner'
+    endGameBanner.textContent = 'You won!' // Announce the winner
+
+    mainDiv.insertAdjacentElement('afterbegin', endGameBanner)
 
     // Add restart button.
 }
