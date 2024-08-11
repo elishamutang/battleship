7fb272d2342001a2218d4e9c15c9cc1ed6844582
elishamutang <elishamutang@outlook.com>
2024-08-11 13:44:13 +0800

    [update] Separated endGame function into its own module.

---
+++ src/endGame.js
@@ -0,0 +1,10 @@
+export default function endGame() {
+    // Announce the winner
+    setTimeout(() => {
+        alert('You won!')
+    }, 200)
+
+    // Disable gameboards..
+
+    // Add restart button..
+}
