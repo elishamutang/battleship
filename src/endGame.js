1abb052179eba8ff2e3af4cf7e5ccaed91b28e79
elishamutang <elishamutang@outlook.com>
2024-08-11 15:15:02 +0800

    [update] Disable gameboards after game ends.

--- src/endGame.js
+++ src/endGame.js
@@ -1,10 +1,18 @@
-export default function endGame() {
+export default function endGame(realPlayerGameboard, computerPlayerGameboard) {
     // Announce the winner
     setTimeout(() => {
         alert('You won!')
     }, 200)
 
-    // Disable gameboards..
+    // Disable gameboards.
+    const allTiles = Array.from(computerPlayerGameboard.getElementsByClassName('loc'))
 
-    // Add restart button..
+    allTiles.forEach((tile) => {
+        tile.className = tile.className + ' no-hover'
+    })
+
+    computerPlayerGameboard.className = computerPlayerGameboard.className + ' gameover'
+    realPlayerGameboard.className = realPlayerGameboard.className + ' gameover'
+
+    // Add restart button.
 }
