67791b77045dd8ceec424f218341abd7ade6adc5
elishamutang <elishamutang@outlook.com>
2024-08-11 17:09:28 +0800

    [update] Added restart button and end game banner.

--- src/endGame.js
+++ src/endGame.js
@@ -1,4 +1,6 @@
-export default function endGame(realPlayerGameboard, computerPlayerGameboard) {
+import generateTheDOM from './theDOM'
+
+export default function endGame(realPlayerGameboard, realPlayer, computerPlayerGameboard, computerPlayer) {
     // Disable gameboards.
     const allTiles = Array.from(computerPlayerGameboard.getElementsByClassName('loc'))
 
@@ -14,9 +16,41 @@ export default function endGame(realPlayerGameboard, computerPlayerGameboard) {
 
     const endGameBanner = document.createElement('div')
     endGameBanner.id = 'endGameBanner'
-    endGameBanner.textContent = 'You won!' // Announce the winner
 
+    const announcement = document.createElement('h1')
+    announcement.id = 'announcement'
+    announcement.textContent = 'You won!' // Announce the winner
+
+    endGameBanner.append(announcement)
     mainDiv.insertAdjacentElement('afterbegin', endGameBanner)
 
     // Add restart button.
+    const restartBtn = document.createElement('button')
+    restartBtn.id = 'restartBtn'
+    restartBtn.textContent = 'Re-match?'
+
+    endGameBanner.append(restartBtn)
+
+    restartBtn.addEventListener('click', (e) => {
+        // Reset gameboards.
+        realPlayer.gameboard.reset()
+        computerPlayer.gameboard.reset()
+
+        // Remove end game banner and gameboards.
+        document.getElementById('endGameBanner').remove()
+
+        realPlayerGameboard.className = 'gameboard'
+        computerPlayerGameboard.className = 'gameboard'
+
+        const realPlayerGameboardChildren = Array.from(realPlayerGameboard.children)
+        const computerPlayerGameboardChildren = Array.from(computerPlayerGameboard.children)
+
+        for (let i = 1; i < realPlayerGameboardChildren.length; i++) {
+            realPlayerGameboardChildren[i].remove()
+            computerPlayerGameboardChildren[i].remove()
+        }
+
+        // Re-generate the DOM.
+        generateTheDOM()
+    })
 }
